0dbf48d78c46cfbf4f925f0e2ec77ccdbcf203df
iwana994994 <iwana994994@gmail.com>
2025-02-28 12:13:11 +0100

    Usavrsavanje

--- admin/config/database.php
+++ admin/config/database.php
@@ -3,20 +3,6 @@
 $con=mysqli_connect("localhost","root","","poslasticarnica");
 
 
-$host="localhost";
-$dbname="poslasticarnica";
-$username="root";
-$password="";
-
-try{
-$pdo= new PDO( "mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
-$pdo-> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-}
-catch(ERRMODE_EXEPTION $e)
-{
-    die("Greska sa bazom".$e.getMessage());
-}
-
 
 
 ?>
--- korisnickaStrana/config/poslasticarnica.sql
+++ korisnickaStrana/config/poslasticarnica.sql
@@ -1,4 +1,4 @@
-`user`-- --------------------------------------------------------
+-- --------------------------------------------------------
 -- Host:                         127.0.0.1
 -- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
@@ -29,55 +29,45 @@ CREATE TABLE IF NOT EXISTS `kontakt` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table poslasticarnica.kontakt: ~10 rows (approximately)
+-- Dumping data for table poslasticarnica.kontakt: ~1 rows (approximately)
 INSERT INTO `kontakt` (`id`, `adresa`, `telefon`, `email`, `working_hours`) VALUES
-	(2, 'fsfds', '3232', 'dfdsfs', '43242'),
-	(3, 'Bulevar Kralja Aleksandra 73, Beograd', '+381 11 1234 567', 'kontakt@example.com', '0'),
-	(4, 'Nemanjina 4, Novi Sad', '+381 21 5678 910', 'novisad@example.com', '0'),
-	(5, 'Knez Mihailova 12, Beograd', '+381 64 321 9876', 'office@example.com', '0'),
-	(6, 'Bulevar Kralja Aleksandra 73, Beograd', '+381 11 1234 567', 'kontakt@example.com', '0'),
-	(7, 'Nemanjina 4, Novi Sad', '+381 21 5678 910', 'novisad@example.com', '0'),
-	(8, 'Knez Mihailova 12, Beograd', '+381 64 321 9876', 'office@example.com', '0'),
-	(9, 'Bulevar Kralja Aleksandra 73, Beograd', '+381 11 1234 567', 'kontakt@example.com', '0'),
-	(10, 'Nemanjina 4, Novi Sad', '+381 21 5678 910', 'novisad@example.com', '0'),
-	(11, 'Knez Mihailova 12, Beograd', '+381 64 321 9876', 'office@example.com', '0');
+	(3, 'Nikole Pasica 28, Nis', '+381 11 1234 567', 'kontakt@example.com', '0');
 
 -- Dumping structure for table poslasticarnica.poruka
 CREATE TABLE IF NOT EXISTS `poruka` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL DEFAULT 0,
   `poruka` text NOT NULL,
   `ime` text NOT NULL,
   `email` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_message_user_id` (`user_id`) USING BTREE,
-  CONSTRAINT `FK_message_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `datum` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table poslasticarnica.poruka: ~0 rows (approximately)
+-- Dumping data for table poslasticarnica.poruka: ~1 rows (approximately)
+INSERT INTO `poruka` (`id`, `poruka`, `ime`, `email`, `datum`) VALUES
+	(9, 'FFFFFFFFFFF', 'FFFF', 'FFFF', '2025-02-28 10:46:23');
 
 -- Dumping structure for table poslasticarnica.proizvod
 CREATE TABLE IF NOT EXISTS `proizvod` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `naziv` varchar(50) NOT NULL DEFAULT '0',
-  `opis` text NOT NULL,
+  `opis` varchar(50) NOT NULL DEFAULT '',
   `cena` double NOT NULL DEFAULT 0,
   `zalihe` int(11) NOT NULL DEFAULT 0,
   `slika` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table poslasticarnica.proizvod: ~9 rows (approximately)
+-- Dumping data for table poslasticarnica.proizvod: ~8 rows (approximately)
 INSERT INTO `proizvod` (`id`, `naziv`, `opis`, `cena`, `zalihe`, `slika`) VALUES
-	(1, 'Čokoladna torta', 'Bogata torta sa crnom i mlečnom čokoladom.', 1500, 10, 'cokoladna.jpg'),
-	(2, 'Voćna torta', 'Lagani krem sa svežim voćem i vanilom.', 1300, 8, 'vocna.jpg'),
-	(3, 'Cheesecake', 'Kremasti cheesecake sa prelivom od jagoda.', 1200, 15, 'cheesecake.jpg'),
-	(4, 'Medena pita', 'Tradicionalna pita sa medom i orasima.', 1000, 12, 'medena.jpg'),
-	(5, 'Tiramisu', 'Italijanski klasik sa kafom i mascarpone sirom.', 1400, 9, 'tiramisu.jpg'),
-	(6, 'Čokoladna torta', 'Torta sa bogatom čokoladnom kremom i čokoladnim preljevom.', 15.99, 30, 'cokoladna_torta.jpg'),
-	(7, 'Voćna torta', 'Osvežavajuća torta sa sezonskim voćem i laganom kremom.', 12.5, 20, 'vocna_torta.jpg'),
-	(8, 'Torta sa kestenom', 'Torta sa kremom od kestena i prahom od kestenovog brašna.', 18, 15, 'torta_sa_kestenom.jpg'),
-	(9, 'Tiramisu', 'Italijanska torta sa slojevima kafe, mascapone kreme i kakao praha.', 20, 25, 'tiramisu.jpg');
+	(3, 'Cheesecake', 'Kremasti cheesecake sa prelivom od jagoda.', 1000, 15, 'korisnickaStrana/public/slike/Cheesecake.jpg'),
+	(4, 'Medena pita', 'Tradicionalna pita sa medom i orasima.', 1000, 12, 'korisnickaStrana/public/slike/med i orasi.jpg'),
+	(5, 'Tiramisu', 'Italijanski klasik sa kafom i mascarpone sirom.', 1400, 9, 'korisnickaStrana/public/slike/tiramisu.jpg'),
+	(6, 'Čokoladna torta', 'Torta sa bogatom čokoladnom kremom i čokoladnim pr', 15.99, 30, 'korisnickaStrana/public/slike/19.jpg'),
+	(7, 'Voćna torta', 'Osvežavajuća torta sa sezonskim voćem i laganom kr', 12.5, 20, 'korisnickaStrana/public/slike/Cheesecake.jpg'),
+	(8, 'Torta sa kestenom', 'Torta sa kremom od kestena i prahom od kestenovog ', 18, 15, 'korisnickaStrana/public/slike/9.jpg'),
+	(9, 'Tiramisu', 'Italijanska torta sa slojevima kafe, mascapone kre', 20, 25, 'korisnickaStrana/public/slike/tiramisu.jpg'),
+	(10, 'tortad', 'asssssssss', 11111, 0, '0');
 
 -- Dumping structure for table poslasticarnica.user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -88,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table poslasticarnica.user: ~1 rows (approximately)
+-- Dumping data for table poslasticarnica.user: ~0 rows (approximately)
 INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
 	(1, 'iwana', 'iwana994994', 'admin');
 
