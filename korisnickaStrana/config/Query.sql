ddc756edd173dd17f59017132721619b1c0f47e7
iwana994994 <iwana994994@gmail.com>
2025-11-17 17:46:46 +0100

    dodata baza

--- korisnickaStrana/config/Query.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `contact_address` (`addres`, `phone`, `email`) VALUES
-('Bulevar Kralja Aleksandra 73, Beograd', '+381 11 1234 567', 'kontakt@example.com'),
-('Nemanjina 4, Novi Sad', '+381 21 5678 910', 'novisad@example.com'),
-('Knez Mihailova 12, Beograd', '+381 64 321 9876', 'office@example.com');
